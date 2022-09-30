b7b4bedb07b37752c6d4619c8cfd3733264bb433
Stefan <satalbot@protonmail.com>
2022-09-30 16:27:30 +0000

    Added drawing of titles for X-axis, Y-axis, and the chart

--- chart-drawer.js
+++ chart-drawer.js
@@ -1,12 +1,48 @@
 
 function drawChart(){
   var newChart = document.createElement("div");
-  $(newChart).addClass("barchart");
-  alert(ChartData.chartSize[0]);
+  $(newChart).addClass("bar-chart");
   $(newChart).width(ChartData.chartSize[0]);
   $(newChart).height(ChartData.chartSize[1]);
 
+  var newYAxis = document.createElement("div");
+  $(newYAxis).addClass("y-axis");
+  $(newChart).append(newYAxis);
+
+  var titleDiv = document.createElement("div");
+  $(titleDiv).addClass("title-div");
+  $(newChart).append(titleDiv);
+
+  var dataDiv = document.createElement("div");
+  $(dataDiv).addClass("data-div");
+  $(newChart).append(dataDiv);
+
+
+  var newXAxis = document.createElement("div");
+  $(newXAxis).addClass("x-axis");
+  $(newChart).append(newXAxis);
+
   $("body").append(newChart);
+
+  // Draw y titles.
+  var newYAxisTitle = document.createElement("h6");
+  $(newYAxisTitle).addClass("y-axis-title");
+  $(newYAxisTitle).text(ChartData.yAxisName);
+  $(".y-axis").append(newYAxisTitle);
+
+  // Draw title.
+  var newChartTitle = document.createElement("h5");
+  $(newChartTitle).addClass("main-title");
+  $(newChartTitle).text(ChartData.chartName);
+  $(".title-div").append(newChartTitle);
+
+  // Draw x & y titles.
+  var newXAxisTitle = document.createElement("h6");
+  $(newXAxisTitle).addClass("x-axis-title");
+  $(newXAxisTitle).text(ChartData.xAxisName);
+  $(".x-axis").append(newXAxisTitle);
+
+
 
   return;
 }
