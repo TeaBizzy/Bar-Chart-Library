f354b6cd62314b3316da250d566bb3af9dbc3eb2
Stefan <satalbot@protonmail.com>
2022-10-04 18:55:48 +0000

    Added barData to ChartData

--- chart-data.js
+++ chart-data.js
@@ -2,5 +2,6 @@ var ChartData = {
   chartTitle: "Average Pets Per House",
   xAxisTitle: "Pets",
   yAxisTitle: "Average",
-  chartSize: [300, 300]
+  chartSize: [300, 300],
+  barData: [{x: 1, y: 5}, {x: 2, y: 10}]
 };
