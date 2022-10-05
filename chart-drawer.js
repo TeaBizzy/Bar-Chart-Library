f9d644f601a75b8caa3bc71a36fdc948030ea0bd
Stefan <satalbot@protonmail.com>
2022-10-05 14:32:33 +0000

    Implemented bar population

--- chart-drawer.js
+++ chart-drawer.js
@@ -7,6 +7,7 @@ function resizeChart(width, height){
   $("#bar-chart").height(height);
 }
 
+// TODO: Needs refactor.
 function populateYAxis(data){
   var minValue = 0;
   var maxValue = 0;
@@ -17,12 +18,13 @@ function populateYAxis(data){
   }
 
   maxValue = Math.ceil(maxValue * 1.05);
-
+  ChartData.maxValue = maxValue;
   // Calculates the increment of the plot points, using a max of 10.
   var plotPointIncrement = (Math.ceil((maxValue - minValue) / 10));
   // Calculates number of plot points.
   var plotPoints = Math.ceil((maxValue - minValue) / plotPointIncrement);
 
+  // TODO: Refactor this mess, perhaps create a template in html?
   // Creates the plot divisions.
   for(var j = plotPoints; j >= 0; j--) {
     var plotPointText = plotPointIncrement * j;
@@ -37,9 +39,19 @@ function populateYAxis(data){
     $(newYPlot).append(newDash);
     $("#y-axis-data").append(newYPlot);
     $(".y-plot").height($("#y-axis-data").height() / (plotPoints + 1));
-
   }
-  console.log(plotPoints);
+}
+
+// Creates bars using the given bar data.
+// TODO: Make this just use the ChartData object.
+function populateBars(bars){
+  for(var i = 0; i < bars.length; i++){
+    var newBar = document.createElement("div");
+    $(newBar).addClass("bar");
+    $(newBar).width(ChartData.barWidth);
+    $(newBar).height($(".data-div").height() * (bars[i].y / ChartData.maxValue));
+    $(".data-div").append(newBar);
+  }
 }
 
 // Used to create all the elements via code. We may want to do that again when the project is complete.
@@ -50,6 +62,7 @@ function drawChart(){
   changeTitle("#y-axis-title", ChartData.yAxisTitle);
   changeTitle("#x-axis-title", ChartData.xAxisTitle);
   populateYAxis(ChartData.barData);
+  populateBars(ChartData.barData);
   return;
 }
 
