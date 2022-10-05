47741a8533339181abf5d969abeb18026cdb7636
Stefan <satalbot@protonmail.com>
2022-10-05 14:31:52 +0000

    Added keys for maxValue, and barWidth

--- chart-data.js
+++ chart-data.js
@@ -3,5 +3,7 @@ var ChartData = {
   xAxisTitle: "Pets",
   yAxisTitle: "Average",
   chartSize: [300, 300],
-  barData: [{x: 1, y: 5}, {x: 2, y: 10}, {x: 3, y: 13}, {x: 4, y: 2}]
+  barData: [{x: 1, y: 5}, {x: 2, y: 10}, {x: 3, y: 13}, {x: 4, y: 2}],
+  barWidth: 20,
+  maxValue: 0
 };
